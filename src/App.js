37ab7b46a9059818d075257ec5e3374853dc1229
Kenny Maeder <kenny-maeder@web.de>
2022-06-10 16:54:40 +0200

    shows pokemon and the shiny sprite

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+class App extends React.Component {
 
-function App() {
+//Constructor
+constructor(props) {
+  super(props);
+
+  this.state = {
+    items: [],
+    DataisLoaded: false
+  };
+}
+
+//Fetch Data from API
+//Fetch all 898 Pokemon (need loop)
+componentDidMount(){
+  fetch(
+    "https://pokeapi.co/api/v2/pokemon/151")
+    .then((res) => res.json())
+    .then((json) => {
+      this.setState({
+        items: json,
+        DataisLoaded: true
+      });
+    })
+}
+
+
+
+render() {
+  const { DataisLoaded, items } = this.state;
+  let pokemonID = 1;
+  if(!DataisLoaded){ return <div>
+    <h1>Please wait some time...</h1> </div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>POKEDEX</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <h1>Pokedex</h1> {
+          <ol key = { items.id } >
+            <h3>{ items.name } </h3>
+            
+            <img src={items.sprites.front_shiny}></img>
+          </ol>
+      }
     </div>
   );
 }
+};
 
 export default App;
